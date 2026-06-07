85416d72fb25af02d19f24268256299948ff3cc8
Gw <revokop09@gmail.com>
2026-06-07 10:05:15 +0530

    Update

--- anikamusic/utils/inline/start.py
+++ anikamusic/utils/inline/start.py
@@ -88,6 +88,14 @@ def private_panel(_):
             ),
         ],
         [
+            # NEW: Website Button (Normal Color, Full Width) - Bada Wala
+            api_btn(
+                text="˹ 𝚼єʙsιᴛє ˼", 
+                url="https://nex0-1.vercel.app", 
+                custom_emoji_id="5262770659267735289" # No style given, defaults to normal color
+            ),
+        ],
+        [
             # My Master (Red/Danger) - Akela Bada Button Niche
             api_btn(
                 text=_["S_B_5"], 
