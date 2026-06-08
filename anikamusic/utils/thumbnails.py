88573a0bf7cbe2b6518cc6dc2fbba3b478a7ca89
Gw <revokop09@gmail.com>
2026-06-08 17:19:38 +0530

    Update

--- anikamusic/utils/thumbnails.py
+++ anikamusic/utils/thumbnails.py
@@ -1,3 +1,3 @@
 async def get_thumb(videoid):
     # Directly uses your raw anime video link
-    return "https://graph.org/file/a44bfdc9a7b6a2b15fa6f-789cfe6541c101bdba.mp4"
+    return "https://files.catbox.moe/v5aubf.mp4"
